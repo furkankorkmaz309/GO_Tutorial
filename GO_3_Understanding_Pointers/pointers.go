8a55df3eae2f6572ed6c67a46029442116147cde
fkorkmazz <furkankrkmaz@hotmail.com>
2024-12-16 12:44:43 +0300

    GO_Tutorial

---
+++ GO_3_Understanding_Pointers/pointers.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	age := 32 // regular variable
+
+	var agePointer *int = &age
+
+	fmt.Println("Age:", agePointer)
+	fmt.Println("Age:", *agePointer)
+
+	editAgeToAdultYears(agePointer)
+	fmt.Println(age)
+
+}
+
+func editAgeToAdultYears(age *int) {
+	//return *age - 18
+	*age = *age - 18
+}

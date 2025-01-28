62c1fbf88539c536996944de3dbb11445a561164
furkankorkmaz309 <furkankrkmaz@hotmail.com>
2025-01-28 15:01:42 +0300

    Initial commit

---
+++ GO_7_Deep_Dive/GO_1_Functions_as_Values/main.go
@@ -0,0 +1,50 @@
+package main
+
+import "fmt"
+
+type transformFn func(int) int
+
+func main() {
+	numbers := []int{1, 2, 3, 4}
+	moreNumbers := []int{5, 1, 2}
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
+	fmt.Println(doubled)
+	fmt.Println(tripled)
+
+	transformFn1 := getTransformerFunction(&numbers)
+	transformFn2 := getTransformerFunction(&moreNumbers)
+
+	transformedNumbers := transformNumbers(&numbers, transformFn1)
+	moreTransformedNumbers := transformNumbers(&moreNumbers, transformFn2)
+
+	fmt.Println(transformedNumbers)
+	fmt.Println(moreTransformedNumbers)
+}
+
+func getTransformerFunction(numbers *[]int) transformFn {
+	if (*numbers)[0] == 1 {
+		return double
+	} else {
+		return triple
+	}
+
+}
+
+func transformNumbers(numbers *[]int, transform transformFn) []int {
+	dnumbers := []int{}
+
+	for _, val := range *numbers {
+		dnumbers = append(dnumbers, transform(val))
+	}
+	return dnumbers
+}
+
+func double(number int) int {
+	return number * 2
+}
+
+func triple(number int) int {
+	return number * 3
+}
